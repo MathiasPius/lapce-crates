7c9b83f2180d692b39e02482f0d99fc8d33cfdd7
Mathias Pius <contact@pius.io>
2024-01-11 00:12:58 +0100

    Bump version to 0.0.6 to track crates-lsp version

--- src/download.rs
+++ src/download.rs
@@ -14,7 +14,7 @@ use crate::{
     version::SelectedVersion,
 };
 
-const LATEST_VERSION: Version = Version::new(0, 0, 4);
+const LATEST_VERSION: Version = Version::new(0, 0, 6);
 
 fn download_into(
     mut file: impl Write,
